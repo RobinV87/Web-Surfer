dbfad8a4152e1459522f81513e7142d97b482040
Robin Versloot <robinversloot1987@gmail.com>
2024-07-04 14:04:09 +0200

    .

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("DOM fully loaded and parsed");
-    var name = "Robin";
-    var nameItem = document.getElementById('name-item');
-    if (nameItem) {
-      console.log("Element found:", nameItem);
-      nameItem.textContent += name;
-    } else {
-      console.log("Element with id 'name-item' not found");
-    }
-  });
+var name = "Robin";
+  document.getElementById('name-item').textContent += name;
