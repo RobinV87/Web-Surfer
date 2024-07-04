5317289c7676b11d5f5f7c0ef3aa14513546655f
Robin Versloot <robinversloot1987@gmail.com>
2024-07-04 14:07:34 +0200

    added age

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 var name = "Robin";
-  document.getElementById('name-item').textContent += name;
+    document.getElementById('name-item').textContent += name;
+
+var age = Date.getFullYear() - 1987;
+    document.getElementById('age').textContent += age;
