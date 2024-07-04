a21f586c44686a799ba6d58d8ea83c045e836167
Robin Versloot <robinversloot1987@gmail.com>
2024-07-04 15:01:24 +0200

    .

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("DOM fully loaded and parsed");
-    
-    var name = "Robin";
+
+    var name = "Robin Versloot";
     var nameElement = document.getElementById('name-item');
     if (nameElement) {
-      console.log("Name element found");
-      nameElement.textContent += name;
+        console.log("Name element found");
+        nameElement.textContent += name;
     } else {
-      console.log("Name element not found");
+        console.log("Name element not found");
     }
-  
+
     var currentYear = new Date().getFullYear();
     var age = currentYear - 1987;
     var ageElement = document.getElementById('age');
     if (ageElement) {
-      console.log("Age element found");
-      ageElement.textContent += age;
+        console.log("Age element found");
+        ageElement.textContent += age;
     } else {
-      console.log("Age element not found");
+        console.log("Age element not found");
     }
 
     var email = "robin@robinversloot.nl"
     var emailElement = document.getElementById('email');
-    if (nameElement) {
+    if (emailElement) {
         console.log("E-mail element found");
         emailElement.textContent += email;
+    } else {
+        console.log("Email element not found");
     }
-    else {
-        console.log("Email element nog found");
-    }
-    }
-  });
+});
